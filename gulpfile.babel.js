a1b77e1a269729de6c2e825e9ed7da563ac66c38
Adam Joseph Cook <adam.j.cook@alliedstrand.com>
2018-08-09 21:18:42 -0500

    chore: add favicon and social card HTML tags and assets
    Add 'gulp-real-favicon' to project to generate favicons,
    add thumbnail assets for Google+, Facebook and Twitter.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,8 +6,10 @@
 
 import BrowserSync from "browser-sync";
 import del from "del";
+import fs from "fs";
 import gulp from "gulp";
 import log from "fancy-log";
+import realFavicon from "gulp-real-favicon";
 import webpack from "webpack";
 import webpackConfig from "./webpack.config.babel";
 
@@ -16,6 +18,8 @@ const browserSync = BrowserSync.create();
 const IS_PRODUCTION = process.env.NODE_ENV === "production";
 
 log.warn("[Gulp] build mode: ", IS_PRODUCTION ? "PRODUCTION" : "DEVELOPMENT");
+
+const FAVICON_DATA_FILE = "favicondata.json";
 
 /**
  * WEBPACK BUNDLE TASK
@@ -62,14 +66,124 @@ gulp.task("dev-server", () => {
       baseDir: "./dist"
     }
   });
-  gulp.watch("./src/**/*", gulp.series("copy", "bundle"));
+  gulp.watch("./src/**/*", gulp.series("bundle", "copy", "inject-favicon"));
+});
+
+/**
+ * GENERATE FAVICONS TASK
+ * -----------------------------------------------------------------------------
+ * Generates all of the favicons and other icon assets from the master favicon
+ * image.
+ */
+gulp.task("generate-favicon", done => {
+  realFavicon.generateFavicon(
+    {
+      masterPicture: "./src/images/master-favicon-512.png",
+      dest: "./dist",
+      iconsPath: "/",
+      design: {
+        ios: {
+          pictureAspect: "backgroundAndMargin",
+          backgroundColor: "#112233",
+          margin: "0%",
+          assets: {
+            ios6AndPriorIcons: false,
+            ios7AndLaterIcons: false,
+            precomposedIcons: false,
+            declareOnlyDefaultIcon: true
+          },
+          appName: "SME Virtual Network"
+        },
+        desktopBrowser: {},
+        windows: {
+          pictureAspect: "noChange",
+          backgroundColor: "#112233",
+          onConflict: "override",
+          assets: {
+            windows80Ie10Tile: false,
+            windows10Ie11EdgeTiles: {
+              small: false,
+              medium: true,
+              big: false,
+              rectangle: false
+            }
+          },
+          appName: "SME Virtual Network"
+        },
+        androidChrome: {
+          pictureAspect: "backgroundAndMargin",
+          margin: "0%",
+          backgroundColor: "#112233",
+          themeColor: "#112233",
+          manifest: {
+            name: "SME Virtual Network",
+            display: "browser",
+            orientation: "notSet",
+            onConflict: "override",
+            declared: true
+          },
+          assets: {
+            legacyIcon: false,
+            lowResolutionIcons: false
+          }
+        },
+        safariPinnedTab: {
+          pictureAspect: "silhouette",
+          themeColor: "#112233"
+        }
+      },
+      settings: {
+        scalingAlgorithm: "Mitchell",
+        errorOnImageTooSmall: false,
+        readmeFile: false,
+        htmlCodeFile: false,
+        usePathAsIs: false
+      },
+      versioning: {
+        paramName: "v",
+        paramValue: "qABA4X8PYp"
+      },
+      markupFile: FAVICON_DATA_FILE
+    },
+    () => {
+      done();
+    }
+  );
+});
+
+/**
+ * INJECT FAVICONS TASK
+ * -----------------------------------------------------------------------------
+ * Injects the generated favicons and icon assets into the `index.html` file.
+ */
+gulp.task("inject-favicon", () =>
+  gulp
+    .src(["dist/*.html"])
+    .pipe(realFavicon.injectFaviconMarkups(JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).favicon.html_code))
+    .pipe(gulp.dest("dist"))
+);
+
+/**
+ * CHECK FOR FAVICON STYLE/SETTING UPDATES TASK
+ * -----------------------------------------------------------------------------
+ * Run a check with the RealFaviconGenerator service (https://realfavicongenerator.net/)
+ * for potential updates for favicons. This is important because from time-to-time,
+ * device, platform and web browsers update their favicon and icon requirements.
+ */
+gulp.task("check-favicon-update", () => {
+  const currentVersion = JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).version;
+  realFavicon.checkForUpdates(currentVersion, err => {
+    if (err) {
+      throw err;
+    }
+  });
 });
 
 /**
  * BUILD TASK
  * -----------------------------------------------------------------------------
  */
-gulp.task("build", gulp.series("clean", "copy", "bundle"));
+gulp.task("build", gulp.series("clean", "generate-favicon", "bundle", "copy", "inject-favicon"));
 
 /**
  * LOCAL SERVER RUN TASK
